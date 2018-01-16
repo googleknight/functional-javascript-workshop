3c6dfbdd2d5a06bacf7966de1bc0184f52b41e40
Shubham Mathur <shubham.mathur@engineer.com>
2018-01-16 10:01:19 +0530

    Changing lowercase to uppercase

---
+++ helloworld.js
@@ -0,0 +1,9 @@
+function upperCaser(word)
+{
+    return word.toUpperCase();
+}
+
+//  module.exports = upperCaser;
+
+//Test Cases
+console.log("Changing lowercase to uppercase",upperCaser("abcd")==="ABCD");
