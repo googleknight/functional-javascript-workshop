b031411255200aac27b9d2e9982e105ac0d414f6
Shubham Mathur <shubham.mathur@engineer.com>
2018-01-17 10:51:14 +0530

    new test case added in reduce

--- reduce.js
+++ reduce.js
@@ -1,5 +1,7 @@
 function countWords(input){
     let result={};
+    if(input.length===0)
+        return null;
     input.reduce(
         function(acc,curVal){
             if(!acc[curVal]){
@@ -23,3 +25,6 @@ let inputWords = ['Apple', 'Banana', 'Apple', 'Durian', 'Durian', 'Durian'];
 let testObject = { Apple: 2, Banana: 1, Durian: 3};
 let resultObject = countWords(inputWords);
 console.log("Count words",resultObject.Apple===2 && resultObject.Banana===1 && resultObject.Durian===3);
+
+resultObject = countWords([]);
+console.log("Count words",resultObject===null);
