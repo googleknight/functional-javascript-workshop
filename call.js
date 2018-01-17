0f5656c7f616515224753a049060f009393f0d39
Shubham Mathur <shubham.mathur@engineer.com>
2018-01-17 21:46:40 +0530

    More test cases added

--- call.js
+++ call.js
@@ -1,5 +1,5 @@
 function duckCount(){
-    arrayofArguments = Array.prototype.slice.call(arguments);
+    let arrayofArguments = Array.prototype.slice.call(arguments);
     return arrayofArguments.filter( function(arg){
         return Object.prototype.hasOwnProperty.call(arg,'quack');
     }).length;
@@ -11,5 +11,9 @@ function duckCount(){
 
 //test cases
 let notDuck = Object.create({quack: true});
-let duck = {quack:false}
-console.log("Number of quacks:",duckCount(duck,notDuck)===1);
+let duck = {quack:false};
+console.log('Number of ducks with quack should be 1:',duckCount(duck,notDuck)===1);
+console.log('Number of ducks with quack should be 2:',duckCount(duck,duck)===2);
+console.log('Number of ducks with quack should be 0 for no arguments:',duckCount()===0);
+console.log('Number of ducks with empty objects, and 1 nonempty should be 1:',duckCount({},{quack:32},{})===1);
+console.log('Number of ducks with zero quack should be 0:',duckCount(notDuck,notDuck)===0);
